a36bbe8810c5efc5589ec8a1d5b37e0dc77b1bae
lucmayor <mayorl@myumanitoba.ca>
2025-06-03 21:17:40 -0500

    adding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 import { readingtime } from './src/pages/api/readingtime.js';
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [readingtime],
   },
-  integrations: [svelte({
-    extensions: ['.svelte']
-  })],
+  output: 'static',
+  // @ts-ignore
+  adapter: vercel(),
+  integrations: [svelte()]
 });
